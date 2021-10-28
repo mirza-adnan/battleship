248b885a52c7e3aaf3c9ab709eed7d2be24a2494
mirza-adnan <mirza.adnan2205@gmail.com>
2021-10-29 00:57:30 +0600

    rewrote the tests

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,40 +1,38 @@
 import Player from "../app/factories/player";
+import GameBoard from "../app/factories/gameboard";
+import Ship from "../app/factories/ship";
 import { SHIPS } from "../app/helpers/data";
 
-let player1, player2;
+let player, gameBoard;
 beforeEach(() => {
-    player1 = Player("human");
-    player2 = Player("computer");
+    player = Player();
+    gameBoard = GameBoard();
 });
 
 describe("Player Factory Function", () => {
     test("toggleTurn inverses turn", () => {
-        expect(player1.turn).toBe(false);
-        player1.toggleTurn();
-        expect(player1.turn).toBe(true);
-        player1.toggleTurn();
-        expect(player1.turn).toBe(false);
+        expect(player.turn).toBe(false);
+        player.toggleTurn();
+        expect(player.turn).toBe(true);
+        player.toggleTurn();
+        expect(player.turn).toBe(false);
     });
 
     test("isAi value is assigned correctly", () => {
-        expect(player1.isAi).toBe(false);
-        expect(player2.isAi).toBe(true);
+        expect(player.isAi).toBe(false);
     });
 
     test("ships array is created", () => {
-        expect(player1.ships.length).toBe(SHIPS.length);
+        expect(player.ships.length).toBe(SHIPS.length);
     });
 
     test("attacks", () => {
-        player1.attack(player2.gameBoard, 4, 5);
-        expect(player2.gameBoard.board[4][5]).toBe(true);
-        player2.gameBoard.placeShip(player2.ships[0], 2, 0);
-        player1.attack(player2.gameBoard, 2, 0);
-        expect(player2.gameBoard.board[2][0].status).toBe("hit");
-    });
+        player.attack(gameBoard, 4, 5);
+        expect(gameBoard.board[4][5]).toBe("miss");
 
-    test("randomly places all ships", () => {
-        player1.placeAllShips();
-        expect(player1.gameBoard.placedShips.length).toBe(player1.ships.length);
+        const ship = Ship("Cruiser", 3);
+        gameBoard.placeShip(ship, 6, 2);
+        player.attack(gameBoard, 6, 2);
+        expect(gameBoard.board[6][2].hit).toBe(true);
     });
 });

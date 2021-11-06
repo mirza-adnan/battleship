e0660029c946040f01609fa3b35dbcb26495d739
mirza-adnan <mirza.adnan2205@gmail.com>
2021-11-07 01:29:36 +0600

    added controlBtns

--- src/app/view/elements.js
+++ src/app/view/elements.js
@@ -2,6 +2,7 @@ const ELEMENTS = {
     p1Grid: document.querySelector(".p1-grid"),
     p2Grid: document.querySelector(".p2-grid"),
     shipsContainer: document.querySelector(".ships-container"),
+    controlBtns: document.querySelector(".control-buttons"),
 };
 
 export default ELEMENTS;

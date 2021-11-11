31a2a42c73ff6a3413702fe885321724ac07332e
mirza-adnan <mirza.adnan2205@gmail.com>
2021-11-11 17:49:09 +0600

    added methods to start, reset and attack

--- src/app/game.js
+++ src/app/game.js
@@ -3,6 +3,7 @@ import GameBoard from "./factories/gameboard";
 import DOM from "./view/dom";
 import Drag from "./view/drag";
 import ELEMENTS from "./view/elements";
+import { SHIPS } from "./helpers/data";
 
 function Game() {
     const player1 = Player(1);
@@ -15,7 +16,7 @@ function Game() {
 
     const changeDirection = () => {
         player1.invertShips();
-        DOM.changeDirection();
+        DOM.changeOrientation();
     };
 
     const autoPlace = () => {
@@ -26,7 +27,7 @@ function Game() {
         p1Drag.addDragDropListeners();
     };
 
-    const reset = () => {
+    const resetShips = () => {
         p1GameBoard.reset();
         player1.resetShips();
         DOM.resetDirection();
@@ -35,9 +36,30 @@ function Game() {
         DOM.showShips();
     };
 
+    const resetGame = () => {
+        player1.reset();
+        player2.reset();
+        p1GameBoard.reset();
+        p2GameBoard.reset();
+
+        DOM.resetGame();
+        DOM.showShips();
+        DOM.renderGrid(p1GameBoard.board, ELEMENTS.p1Grid);
+        DOM.renderGrid(p2GameBoard.board, ELEMENTS.p2Grid);
+        DOM.hideModal();
+
+        p1Drag.addGridCellListeners();
+    };
+
     const toggleTurns = () => {
         player1.toggleTurn();
         player2.toggleTurn();
+    };
+
+    const closeModal = (e) => {
+        if (e.target.classList.contains("modal-bg")) {
+            DOM.hideModal();
+        }
     };
 
     const attackHandler = (e) => {
@@ -56,42 +78,49 @@ function Game() {
 
         if (p2GameBoard.areShipsSunk()) {
             const text = "Player 1 Wins";
+            DOM.showPlayAgainButton();
             DOM.showModal(text);
+            return;
         }
 
         if (player2.turn) {
             player2.randomAttack(p1GameBoard);
-            setTimeout(
-                DOM.renderGrid(p1GameBoard.board, ELEMENTS.p1Grid, false),
-                3000
-            );
-            toggleTurns();
+            setTimeout(() => {
+                DOM.renderGrid(p1GameBoard.board, ELEMENTS.p1Grid, false);
+                player1.toggleTurn();
+            }, 250);
+            player2.setTurn(false);
         }
 
         if (p1GameBoard.areShipsSunk()) {
             const text = "Player 2 Wins";
+            DOM.showPlayAgainButton();
             DOM.showModal(text);
+            return;
         }
     };
 
     const startGame = () => {
-        addAttackListeners();
-        player2.autoPlaceAllShips(p2GameBoard);
-        DOM.startGame();
+        if (p1GameBoard.placedShips.length === SHIPS.length) {
+            addAttackListeners();
+            player2.autoPlaceAllShips(p2GameBoard);
+            DOM.startGame();
+        } else {
+            DOM.hidePlayAgainButton();
+            DOM.showModal("Place all your ships");
+        }
     };
 
-    const addButtonListeners = () => {
-        const changeDirBtn = document.querySelector(".change-direction");
-        changeDirBtn.addEventListener("click", changeDirection);
-
-        const autoPlaceBtn = document.querySelector(".auto-place");
-        autoPlaceBtn.addEventListener("click", autoPlace);
-
-        const resetBtn = document.querySelector(".reset");
-        resetBtn.addEventListener("click", reset);
-
-        const startBtn = document.querySelector(".start-btn");
-        startBtn.addEventListener("click", startGame);
+    const addEventListeners = () => {
+        ELEMENTS.changeOrientationBtn.addEventListener(
+            "click",
+            changeDirection
+        );
+        ELEMENTS.autoPlaceBtn.addEventListener("click", autoPlace);
+        ELEMENTS.resetBtn.addEventListener("click", resetShips);
+        ELEMENTS.startBtn.addEventListener("click", startGame);
+        ELEMENTS.modalBg.addEventListener("click", closeModal);
+        ELEMENTS.playAgainBtn.addEventListener("click", resetGame);
     };
 
     const addAttackListeners = () => {
@@ -106,7 +135,7 @@ function Game() {
         DOM.renderGrid(p2GameBoard.board, ELEMENTS.p2Grid, true);
         DOM.createShips(ELEMENTS.shipsContainer);
         p1Drag.addDragDropListeners();
-        addButtonListeners();
+        addEventListeners();
     };
 
     return Object.freeze({
